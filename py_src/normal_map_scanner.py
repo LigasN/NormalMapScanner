e9bd13d602c44b0d858d46a962fddea204c02c27
Norbert Ligas <norbert221198@gmail.com>
2022-01-16 23:21:33 +0100

    reading input data py

--- py_src/normal_map_scanner.py
+++ py_src/normal_map_scanner.py
@@ -1,16 +1,23 @@
+import numpy as np
+from tkinter import BitmapImage
 from PIL import Image
 
 
 R, G, B = 0, 1, 2
-
-
-def load_image_BMP2RGB(filepath, filename):
-    im = Image.open("../assets/input_0.bmp")
-    im.split()
+angle = np.array(range(0, 360, 45))
 
 
 def main():
-    load_image_BMP2RGB("../assets /", "input_0.bmp")
+    # Load input images
+    input_images = np.empty(angle.size, dtype=BitmapImage)
+    for i in range(0, angle.size):
+        input_images[i] = (Image.open(
+            "../assets/input_" + str(angle[i]) + ".bmp"))
+
+    # Save output image
+    output = input_images[3]
+    output.show()
+    output.save("./normalmap.bmp")
 
 
 if __name__ == "__main__":
