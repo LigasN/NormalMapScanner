a955f253e1d686605bec46d54e38e89ac75a8e41
Norbert Ligas <norbert221198@gmail.com>
2022-01-19 23:27:38 +0100

    beginning of the algorithm

--- py_src/normal_map_scanner.py
+++ py_src/normal_map_scanner.py
@@ -5,19 +5,31 @@ from PIL import Image
 
 R, G, B = 0, 1, 2
 angle = np.array(range(0, 360, 45))
+Light_intenisty_W = 2  # Watts
+Light_intenisty_lumens = 220  # lumens
 
 
 def main():
-    # Load input images
+    print('Loading input images.')
     input_images = np.empty(angle.size, dtype=BitmapImage)
     for i in range(0, angle.size):
-        input_images[i] = (Image.open(
-            "../assets/input_" + str(angle[i]) + ".bmp"))
+        try:
+            input_images[i] = Image.open(
+                '../assets/input_' + str(angle[i]) + '.bmp')
+        except FileNotFoundError:
+            print('File not found: ../assets/input_' + str(angle[i]) + '.bmp')
 
+    # Create output image object
+    output = Image.new('RGB', (1200, 1200))
+    angles = np.empty(1)
+
+    for i in range(0, angle.size):
+        angles[i] = np.arccos()
+    print(angles)
     # Save output image
-    output = input_images[3]
-    output.show()
-    output.save("./normalmap.bmp")
+    # output.show()
+    print('Saving')
+    # output.save("./tmp/normalmap.bmp")
 
 
 if __name__ == "__main__":
