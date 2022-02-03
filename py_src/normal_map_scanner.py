5b865d8d6d58e30a5ef81915cae02ef1d9d886c2
Norbert Ligas <norbert221198@gmail.com>
2022-02-03 22:19:04 +0100

    something - conversion to PIL, weight way to calculate normals

--- py_src/normal_map_scanner.py
+++ py_src/normal_map_scanner.py
@@ -1,3 +1,4 @@
+from email import message
 import numpy as np
 import math
 import time
@@ -27,8 +28,12 @@ def sRGB2Linear(im):
     return Image.fromarray(arrOut)
 
 
-def vec2_mag(vec2):
-    return math.sqrt(vec2[0]**2 + vec2[1]**2)
+def magnitude(v):
+    return np.sqrt(np.sum(v**2))
+
+
+def normalize(v):
+    return v / magnitude(v)
 
 
 def load_image(filename):
@@ -62,10 +67,12 @@ def main():
             im, environment_light)
 
         # Sum colors
-        for y, x in np.ndindex(image_size[1], image_size[0]):
-            input[a][y][x] = float(np.sum(im.getpixel(
-                (x, y))))
-        print("Loaded and converted in %d sec" % (time.time() - start_time))
+        input[a] = (np.array(im.getdata(R), float) +
+                    np.array(im.getdata(G), float) +
+                    np.array(im.getdata(B), float)).reshape(
+            image_size[1], image_size[0])
+        print("Loaded and converted in %d ms" %
+              ((time.time() - start_time) * 1000))
 
     print('Calcultion of the positions of the lamps')
     start_time = time.time()
@@ -80,48 +87,41 @@ def main():
             lamp_pos[angle_idx][1] = np.sin(
                 angles_rad[angle_idx]) * lamp_distance
             lamp_pos[angle_idx][2] = lamp_0_position[2]
-    print("Position of lamps calculated in %d sec" %
-          (time.time() - start_time))
+    print("Position of lamps calculated in %d ms" %
+          ((time.time() - start_time) * 1000))
 
-    # TODO: calculation is done in the wrong way
-    # It should be N = w1*v1 + w2*v2 + ...
     print('Calculation of the normalmap vectors')
+    start_time = time.time()
     pixel_size = object_size / image_size
     pixel_idx = np.zeros(2)
     for y, x in np.ndindex(image_size[1], image_size[0]):
         # (pixel position for an object aligned with its upper left corner
-        # to (0,0)) -/+ to center results back)
+        # to (0,0)) and -/+ to center results back)
         pixel_idx = np.array((x, y))
         pixel_pos = ((pixel_idx * pixel_size) +
                      (np.array((pixel_size[0], (-pixel_size[1]))) / 2)) - (np.array((object_size[0], (-object_size[1]))) / 2)
         pixel_pos = np.append(pixel_pos, 0.)  # Z axis
 
-        N_vectors = np.zeros((8, 3))
+        N_vector = np.zeros(3, float)
         for angle_idx in range(angles_rad.size):
             # Vector pointing to the light source
             L_vector = lamp_pos[angle_idx] - pixel_pos
             # L_vector is pointing to the lamp. Input image for normal vector
             # equal to this should be white (255,255,255). For opposite
             # vector color would be black (0,0,0).
-            weight = input[angle_idx][y][x] / 765.
-            NL_angle = (1 - weight) * np.pi
-            # To move a vector without using quaterions it is moved on its axis created parallel to diagonal of X and Y values of the vector
-            diagonal = vec2_mag(L_vector[:2])
-            dz_mag = vec2_mag((diagonal, L_vector[2]))
-            new_diagonal = np.cos(
-                NL_angle) * dz_mag  # x axis
-            N_vectors[angle_idx][2] = np.sin(
-                NL_angle) * dz_mag  # y axis
-            # Diagonal change ratio to get x and y back
-            diag_ratio = new_diagonal / diagonal
-            N_vectors[angle_idx][:2] = diag_ratio * L_vector[:2]
-        # Averaging of the N vector results
-        N_vector = np.average(N_vectors, axis=0)
-        output[int(pixel_idx[1])][int(pixel_idx[0])][0] = N_vector[0]
+            weight = input[angle_idx][y][x] / 765.0
+            N_vector += L_vector * weight
+        N_vector = normalize(N_vector)
+        output[y][x] = N_vector
+    print("Normalmap vectors calculated in %d ms" %
+          ((time.time() - start_time) * 1000))
 
-    # Convert back to PIL
-    # TODO: There need to be some sort of conversion to take normals into image space
-    normalmap = Image.fromarray(output, mode='RGB')
+    print('Conversion back to PIL')
+    start_time = time.time()
+    image_data = np.array((((output + 1.0) / 2.0) * 255.0), int)
+    normalmap = Image.fromarray(image_data, mode='RGB')
+    print("Convertion back to PIL done in %d ms" %
+          ((time.time() - start_time) * 1000))
 
     # Create output image object
     normalmap.show()
