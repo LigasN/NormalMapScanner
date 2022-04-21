da6b3c8dfe7f2a6137746d3725bfaac9fccedf75
LigasN <norbert221198@gmail.com>
2022-04-21 20:51:53 +0200

    cleanup after change to raspberry

--- docs/worklog.md
+++
@@ -1,3 +0,0 @@
-# Worklog
-## 28.10.2021
-Repository is created with .gitignore, readme and worklog.
--- py_src/normal_map_scanner.py
+++ py_src/normal_map_scanner.py
@@ -8,34 +8,36 @@ import datetime
 # Properties
 # debugging = 1 # True
 debugging = 0  # False
-# test = "Real input"
-# test = "All black input"
-# test = "All white input"
-test = "Photos"
-if(test == "Real input"):
-    image_size = np.array([1200, 1200])  # px
-    object_size = np.array([20, 20])  # cm
-    assets_path = '../assets/'
-    lamp_0_position = np.array([40, 0, 15])  # cm
-elif(test == "All black input"):
+# test = "All black 3x3 px"
+# test = "All white 3x3 px"
+# test = "Simple test 3x3 px"
+# test = "Blender 1200x1200 px"
+test = "Photos 2200x2200 px"
+
+if(test == "All black 3x3 px"):
+    assets_path = 'test_input/1.all_black_3x3px/' # test assets location
     image_size = np.array([3, 3])  # px test image size
     object_size = np.array([3, 3])  # cm virtual test object size
-    assets_path = '../assets/test_assets/all_black/'  # test assets location
     lamp_0_position = np.array([40, 0, 15])  # cm
-elif(test == "All white input"):
+elif(test == "All white 3x3 px"):
+    assets_path = 'test_input/2.all_white_3x3px/' # test assets location
     image_size = np.array([3, 3])  # px test image size
     object_size = np.array([3, 3])  # cm virtual test object size
-    assets_path = '../assets/test_assets/all_white/'  # test assets location
     lamp_0_position = np.array([0, 0, 15])  # cm
-elif(test == "Simple test"):
+elif(test == "Simple test 3x3 px"):
+    assets_path = 'test_input/3.simple_test_3x3px/' # test assets location
     image_size = np.array([4, 4])  # px test image size
     object_size = np.array([4, 4])  # cm virtual test object size
-    assets_path = '../assets/test_assets/simple_test/'  # test assets location
     lamp_0_position = np.array([40, 0, 15])  # cm
-elif(test == "Photos"):
+elif(test == "Blender 1200x1200 px"):
+    assets_path = 'test_input/4.blender_1200x1200px/' # test assets location
+    image_size = np.array([1200, 1200])  # px
+    object_size = np.array([20, 20])  # cm
+    lamp_0_position = np.array([40, 0, 15])  # cm
+elif(test == "Photos 2200x2200 px"):
+    assets_path = 'test_input/5.photos_2200x2200px/' # test assets location
     image_size = np.array([2200, 2200])  # px test image size
     object_size = np.array([20, 20])  # cm virtual test object size
-    assets_path = '../assets/test_assets/photos/'  # test assets location
     lamp_0_position = np.array([40, 0, 22])  # cm
 
 
